51dc27dc11b4b53133bf711367d21f7a68b29566
Bohdan .K <spudegbutman@gmail.com>
2021-05-13 16:14:36 +0300

    add: hz

--- src/components/MainPage/MainAbout/MainAbout.tsx
+++ src/components/MainPage/MainAbout/MainAbout.tsx
@@ -1,29 +1,69 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Container from '../../Container/Container';
 import Paragraph from '../../Paragraph/Paragraph';
 import SectionHeader from '../../SectionHeader/SectionHeader';
 import './MainAbout.scss';
+import Image from 'gatsby-image';
 import ArrowSVG from '../../../images/svg/mainPage/arrowMore.inline.svg';
+import { graphql, useStaticQuery } from 'gatsby';
+import Carousel from '../../Carousel/Carousel';
+import { MainAboutQueryTypes } from './Types';
+import ArrowNextSVG from '../../../images/svg/mainPage/arrowNext.inline.svg';
+import ArrowPrevSVG from '../../../images/svg/mainPage/arrowPrev.inline.svg';
+
+const MAIN_ABOUT_US_QUERY = graphql`
+	query MainAboutUsQuery {
+		allFile(filter: { relativePath: { regex: "/images/mainAboutUs/" } }) {
+			nodes {
+				absolutePath
+				childImageSharp {
+					fluid {
+						...GatsbyImageSharpFluid_withWebp
+					}
+				}
+				relativePath
+				relativeDirectory
+			}
+		}
+	}
+`;
 
 const MainAbout: React.FC = () => {
+	const {
+		allFile: { nodes },
+	}: MainAboutQueryTypes = useStaticQuery(MAIN_ABOUT_US_QUERY);
+
+	const [detailsOpen, setDetailsOpen] = useState<boolean>(false);
+
+	const handleDetails = () => {
+		setDetailsOpen((prev) => !prev);
+	};
+
 	return (
 		<section className="main-about-us">
 			<Container>
 				<SectionHeader>О нас</SectionHeader>
-				<div className="main-about-us__text-wrap">
-					<Paragraph>
+				<div className="main-about-us__wrapper">
+					<Paragraph classNames="main-about-us__text">
 						Задача организации, в особенности же укрепление и развитие структуры требуют определения и уточнения дальнейших направлений
 						развития. Не следует, однако забывать, что дальнейшее развитие различных форм деятельности обеспечивает широкому кругу
-						(специалистов) участие в формировании системы обучения кадров, соответствует насущным потребностям Идейные соображения высшего
-						порядка, а также укрепление и развитие структуры влечет за собой процесс внедрения и модернизации направлений прогрессивного
-						развития. Повседневная практика показывает, что постоянный количественный рост и сфера нашей активности требуют от нас анализа
-						позиций, занимаемых участниками в отношении поставленных задач. С другой стороны новая модель организационной деятельности
-						представляет собой интересный эксперимент проверки форм развития.
+						(специалистов) участие в формировании системы обучения кадров, соответствует насущным потребностям
 					</Paragraph>
+					<Paragraph classNames={`main-about-us__text ${detailsOpen ? 'main-about-us__text--open' : ''}`}>
+						Идейные соображения высшего порядка, а также укрепление и развитие структуры влечет за собой процесс внедрения и модернизации
+						направлений прогрессивного развития. Повседневная практика показывает, что постоянный количественный рост и сфера нашей
+						активности требуют от нас анализа позиций, занимаемых участниками в отношении поставленных задач. С другой стороны новая модель
+						организационной деятельности представляет собой интересный эксперимент проверки форм развития.
+					</Paragraph>
+					<button className="main-about-us__btn-more" onClick={handleDetails}>
+						подробнее <ArrowSVG className={`main-about-us__btn-svg ${detailsOpen ? 'main-about-us__btn-svg--open' : ''}`} />
+					</button>
+					<Carousel buttonPrev={<ArrowPrevSVG />} buttonNext={<ArrowNextSVG />}>
+						{nodes.map(({ childImageSharp: { fluid } }, i) => (
+							<Image key={`${i}+${fluid}`} fluid={fluid}></Image>
+						))}
+					</Carousel>
 				</div>
-				<button className="main-about-us__btn-more">
-					подробнее <ArrowSVG className="main-about-us__btn-svg" />
-				</button>
 			</Container>
 		</section>
 	);
