3998245bd5d64503620be314e8750da2338a573a
Bohdan .K <spudegbutman@gmail.com>
2021-05-13 13:37:45 +0300

    add: faq

---
+++ src/components/MainPage/MainFaq/MainFaq.tsx
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import { mainFaqTabs } from '../../../constants/mainFaqTabs';
+import Container from '../../Container/Container';
+import Paragraph from '../../Paragraph/Paragraph';
+import SectionHeader from '../../SectionHeader/SectionHeader';
+import Tabs, { TabHeader } from '../../Tabs/Tabs';
+import './MainFaq.scss';
+
+const MainFaq: React.FC = () => {
+	const [activeTab, setActiveTab] = useState(0);
+	const [activeQuestion, setActiveQuestion] = useState<number | null>(null);
+
+	return (
+		<div className="main-faq">
+			<Container>
+				<SectionHeader>частые вопросы</SectionHeader>
+				<Tabs>
+					<div className="tabs__header">
+						{mainFaqTabs.map(({ id, title }) => (
+							<TabHeader key={id} onItemClicked={() => setActiveTab(id)} isActive={activeTab === id}>
+								{title}
+							</TabHeader>
+						))}
+					</div>
+					<div className="tabs__content">
+						{mainFaqTabs[activeTab].faqList.map(({ id, faqQuestion, faqAnswer }, i) => (
+							<div
+								key={id}
+								className={`tabs__content-item ${activeQuestion === i ? 'tabs__content-item--active' : ''}`}
+								onClick={() => setActiveQuestion(activeQuestion === i ? null : i)}
+							>
+								<div className="tabs__content-item-toggler-wrapper">
+									<div className="tabs__content-item-toggler"></div>
+									<p className="tabs__content-item-title">{faqQuestion}</p>
+								</div>
+								<Paragraph>{faqAnswer}</Paragraph>
+							</div>
+						))}
+					</div>
+				</Tabs>
+			</Container>
+		</div>
+	);
+};
+
+export default MainFaq;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,7 @@ import MainAboutMain from '../components/MainPage/MainAboutMain/MainAboutMain';
 import MainWhyUse from '../components/MainPage/MainWhyUse/MainWhyUse';
 import MainPromo from '../components/MainPage/MainPromo/MainPromo';
 import MainStart from '../components/MainPage/MainStart/MainStart';
+import MainFaq from '../components/MainPage/MainFaq/MainFaq';
 
 const IndexPage: React.FC = (): JSX.Element => {
 	return (
@@ -15,6 +16,7 @@ const IndexPage: React.FC = (): JSX.Element => {
 			<MainAdvantages />
 			<MainWhyUse />
 			<MainStart />
+			<MainFaq />
 		</>
 	);
 };
