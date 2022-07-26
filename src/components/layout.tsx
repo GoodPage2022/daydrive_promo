ed07ac634dbd11ff3fcd2ba44f86c7b1f8e6a6a1
VadymDenysiuk <80970410371q@gmail.com>
2022-07-26 12:55:35 +0300

    fix styles and add title

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Helmet from 'react-helmet';
 import Footer from './Footer/Footer';
 import Header from './Header/Header';
 import './layout.scss';
@@ -6,6 +7,7 @@ import './layout.scss';
 const Layout: React.FC = ({ children }): JSX.Element => {
 	return (
 		<>
+			<Helmet title={'DayDrive | Мобільне рішення для Вашого авто'} />
 			<Header />
 			<main>{children}</main>
 			<Footer />
